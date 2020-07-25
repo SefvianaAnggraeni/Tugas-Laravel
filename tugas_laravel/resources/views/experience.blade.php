0df8db956083cff85a3903caf980a4772af988b5
SefvianaAnggraeni <63381365+SefvianaAnggraeni@users.noreply.github.com>
2020-07-25 17:57:06 +0700

    Add files via upload

---
+++ tugas_laravel/resources/views/experience.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>MY EXPERIENCE</title>
+  </head>
+  <body style="background-color:#FFFFFF;">
+  <center>
+  <div class="container">
+  <br>
+  <h2>MY EXPERIENCE</h2>
+  <hr>
+  <br>
+  <div class="text-center">
+  <img src="permata bank.jpg" class="rounded left" alt="..." width="300px" height="300px">
+  <img src="permata bank (2).jpg" class="rounded right" alt="..." width="300px" height="300px">
+  </div>
+  <br>
+  <p>Saya mengikuti seminar yang diadakan oleh Perusahaan Permata Bank dalam bidang Software yang berlokasi di Jakarta.</p>
+  <br>
+  <div class="text-center">
+  <img src="kunjin.jpg" class="rounded" alt="..." width="300px" height="300px">
+  </div>
+  <br>
+  <p>Saya dan kelompok saya melakukan KUNJIN di bidang SOFTWARE DEVELOPMENT. Di PT. Spasi Indonesia yang berlokasi di The Royal Palace Blok B No.29, Jl. Prof. Dr. Soepomo, SH No. 178 A, Menteng Dalam, Tebet, RT.1/RW.15, Menteng Dalam, Tebet, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12870</p>
+  <br>
+  <h5>Thank You ^_^</h5>
+  </center>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ tugas_laravel/resources/views/portopolio.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>MY PORTOPOLIO</title>
+  </head>
+  <body style="background-color:#FFFFFF;">
+  <center>
+  <div class="container">
+  <br>
+  <h2>MY PORTOPOLIO</h2>
+  <hr>
+  <br>
+  <h4>Aplikasi Form Input Peminjaman Perpustakaan</h4>
+  <br>
+  <div class="text-center">
+  <img src="Screenshot (153).png" class="rounded" alt="..." width="700px" height="600px">
+  </div>
+  <br>
+  <h4>Aplikasi Kasir</h4>
+  <br>
+  <h6>Tampilan Login untuk admin dan kasir sama yang membedakan adalah username dan password.</h6>
+  <div class="text-center">
+  <img src="Capture.PNG" class="rounded" alt="..." width="400px" height="400px">
+  </div>
+  <br>
+  <h6>Bagian admin</h6>
+  <h6>Tampilan menu admin</h6>
+  <div class="text-center">
+  <img src="Capture 1.PNG" class="rounded" alt="..." width="400px" height="400px">
+  </div>
+  <br>
+  <h6>Tampilan form input pegawai</h6>
+  <div class="text-center">
+  <img src="Capture 2.PNG" class="rounded" alt="..." width="400px" height="400px">
+  </div>
+  <br>
+  <h6>Tampilan form input barang</h6>
+  <div class="text-center">
+  <img src="Capture 3.PNG" class="rounded" alt="..." width="400px" height="400px">
+  </div>
+  <br>
+  <h6>Bagian Kasir</h6>
+  <h6>Tampilan menu kasir</h6>
+  <div class="text-center">
+  <img src="Capture 4.PNG" class="rounded" alt="..." width="400px" height="400px">
+  </div>
+  <br>
+  <h6>Tampilan form cari barang</h6>
+  <div class="text-center">
+  <img src="Capture 5.PNG" class="rounded" alt="..." width="400px" height="400px">
+  </div>
+  <br>
+  <h6>Tampilan form input beli barang</h6>
+  <div class="text-center">
+  <img src="Capture 6.PNG" class="rounded" alt="..." width="400px" height="400px">
+  </div>
+  <br>
+  <h5>Thank You ^_^</h5>
+  </center>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  </body>
+</html>
